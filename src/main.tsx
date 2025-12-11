ba142f19dac2a02fd4ff417d71297de35f8c67ac
xiejingyang <136419808@qq.com>
2025-12-11 10:38:31 +0800

    feat: 添加 react-scan 依赖并初始化扫描功能

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import { scan } from "react-scan";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+scan({
+  enabled: import.meta.env.DEV,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
